82ca058bfa83347d7125a19168a6c29b8bfb0d7f
fanimator12 <torok.franciska.leonora@gmail.com>
2021-03-26 14:54:36 +0100

    add

---
+++ Ex1/Ex1.py
@@ -0,0 +1,39 @@
+#print(4.5, 'end', "Hello again", False, end="\n")
+print("Hello World!")
+
+name = input('Name: ')
+
+if name == 'Tim':
+    print("You are great")
+elif name == 'Joe':
+    print("Bye Joe")
+else:
+    print("Always do this")
+#age = input('Age: ')
+
+#print(name)
+
+#print('Hello', name, 'you are', age, 'old')
+
+x = 10
+y = 3
+
+result = int(x/y)
+print(result,',', x ** y, ',', x // y, ',', x % y)
+
+#num = input('Number: ')
+#print(int(num) - 5)
+
+hello = 'heLLo'
+print(hello.lower())
+print(hello.upper())
+print(hello.capitalize())
+print(hello.lower().count('ll'))
+
+z = 'hello'
+v = ' sup'
+print(z + v)
+
+print('a' > 'Z')
+print(ord('a'))
+
